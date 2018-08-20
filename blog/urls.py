0186de9c43fe661f88cc8142b7b0380663535947
Vaibhav Rai <raivaibhav08@gmail.com>
2018-08-20 17:49:03 +0530

    Update Django

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path, include, re_path
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', login , {'template_name': 'blog/login.html'}),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    re_path(r'^register/$', views.register, name='register'),
+    re_path(r'^login/$',  LoginView.as_view(template_name='blog/login.html'), name="login"),
 ]
